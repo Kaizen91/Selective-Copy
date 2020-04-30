0cebbaac4e224e1d7533b3fa5016fc008c8788fd
Stephen Lang <stephenlang91@gmail.com>
2020-04-30 19:06:06 -0400

    initial commit

---
+++ selectiveCopy.py
@@ -0,0 +1,5 @@
+#! /Users/stephenlang/anaconda/bin/python
+# selectiveCopy.py - Walks through a folder tree and searches for files with a certain 
+# file extension (such as .pdf or .jpeg).  These files will be copied from whatever
+# location they are in to a new folder.
+

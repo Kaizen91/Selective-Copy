80b4e34055ef204ff297d142227cc5caf314b766
Stephen Lang <stephenlang91@gmail.com>
2020-04-30 19:22:09 -0400

    added README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+The following is a program that was desinged to take in a source directory, and a target directory  as strings, and a file extension. It will then copy all the files using the given file extension from the source to the target directory.
+
+It was built to demonstrate understanding of Python's shutuile module.
--- selectiveCopy.py
+++ selectiveCopy.py
@@ -3,3 +3,9 @@
 # file extension (such as .pdf or .jpeg).  These files will be copied from whatever
 # location they are in to a new folder.
 
+import os, shutil
+
+#TODO: Take in a folder and a file extension as arguments
+#TODO: Walk the folder tree
+#TODO: copy the files that have the given extension
+    #use shutil.copy as 

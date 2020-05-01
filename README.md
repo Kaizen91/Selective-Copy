7fd56cdbcad7ad4e31f6009d5878787e898ab763
Stephen Lang <stephenlang91@gmail.com>
2020-04-30 20:37:53 -0400

    added instructions to Readme

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 The following is a program that was desinged to take in a source directory, and a target directory  as strings, and a file extension. It will then copy all the files using the given file extension from the source to the target directory.
 
 It was built to demonstrate understanding of Python's shutuile module.
+
+Instructions:
+
+1.download the git repository
+2.open then terminal and change directory into the Selective Copy directory
+3.run "chmod +x selectiveCopy.py"
+4.run "./selectiveCopy.py"
+5.note that files have moved from the Source Folder to the destination Folder
--- selectiveCopy.py
+++ selectiveCopy.py
@@ -3,9 +3,34 @@
 # file extension (such as .pdf or .jpeg).  These files will be copied from whatever
 # location they are in to a new folder.
 
-import os, shutil
+import os, shutil, re
 
 #TODO: Take in a folder and a file extension as arguments
 #TODO: Walk the folder tree
+    #use os.walk
+#TODO: Make a regex to match the file extention
 #TODO: copy the files that have the given extension
-    #use shutil.copy as 
+    #use shutil.copy(source,destination)
+
+def selectiveCopy(source, destination, extention):
+    #check source validity
+    if os.path.isdir(source) and os.path.isdir(destination):
+        #create regex
+        extRegex = re.compile(r'.*(\.'+extention+')')
+
+        #loop through the Source Directory
+        for foldername, subfolders, filenames in os.walk(source):
+            print('The current folder is: '+ foldername)
+            for filename in filenames:
+                mo = extRegex.search(filename)
+
+                #Skip files without the correct extention
+                if mo == None:
+                    continue
+                    
+                #move files that match the extention
+                shutil.copy(os.path.join(source,filename),destination)
+    else:
+        print('The source and/or the destination provided does not exist.')
+
+selectiveCopy(os.path.join('.','Source'),os.path.join('.','Destination'),'txt')
